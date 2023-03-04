a09325f80cfd7e1c9fd145c696a55a49c766f9d6
manuel-spec <amaniase084@gmail.com>
2023-03-04 10:06:09 +0300

    090

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Increase traffic Nginx server can take.
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+
+# Restart server.
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
